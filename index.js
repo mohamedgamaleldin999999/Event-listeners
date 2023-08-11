8d48a2f8199aa1578799b5ab9f09863dbf154872
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-11 14:09:05 +0300

    Prevent default

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ const output = document.querySelector("#output");
 textBox.addEventListener("keydown", (event) => {
   output.textContent = `You pressed "${event.key}".`;
 });
+
+const form = document.querySelector("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const para = document.querySelector("p");
+para.style.color = 'red'
+
+form.addEventListener("submit", (e) => {
+  if (fname.value === "" || lname.value === "") {
+    e.preventDefault();
+    para.textContent = "You need to fill in both names!";
+  }else if(fname.value[0] === 'm' || fname.value[0] === 'M') {
+    e.preventDefault();
+    para.textContent = 'M is not a supported initial'
+  }
+});
