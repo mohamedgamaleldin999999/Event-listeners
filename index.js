814cd60d158b479eff09e988b420a3ebbf72a791
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-11 13:58:03 +0300

    Keydown

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const btn = document.querySelector('button');
 
-      function random(number) {
-        return Math.floor(Math.random() * (number+1));
-      }
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
 
-      function changeBackground(e) {
-        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-        e.target.style.backgroundColor = rndCol;
-        console.log(e)
-      }
+function changeBackground(e) {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  e.target.style.backgroundColor = rndCol;
+  console.log(e)
+}
 
-      btn.addEventListener('click', changeBackground)
+btn.addEventListener('click', changeBackground)
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
